427a627c035314fb0f2d44286127f6f625fa5eec
Colin Gillespie <csgillespie@gmail.com>
2013-11-08 08:36:20 +0000

    Removing tests and adding a simple example

--- goodpackage/R/fakepackage.r
+++ goodpackage/R/fakepackage.r
@@ -1,9 +1,8 @@
-#' Returns 3.
-three <- function() {
+#' An example function
+#' @export
+#' @examples
+#' three() + three()
+three = function() {
   3
 }
 
-#' Check to see if our argument is three.
-is_three <- function(x) {
-  see_if(are_equal(3, x))
-}
